5ee9da1b548ffaa773670f78bd386118e2e09c3d
wodencafe <wodencafe@gmail.com>
2024-08-25 01:28:45 -0500

    feat: Add ManaCost, CardType

---
+++ app/src/main/java/chat/llamas/jmtg/domain/CardType.java
@@ -0,0 +1,21 @@
+package chat.llamas.jmtg.domain;
+
+public enum CardType {
+	LAND(CardTypeType.PERMANENT),
+	CREATURE(CardTypeType.PERMANENT),
+	ARTIFACT(CardTypeType.PERMANENT),
+	ENCHANTMENT(CardTypeType.PERMANENT),
+	PLANESWALKER(CardTypeType.PERMANENT),
+	BATTLE(CardTypeType.PERMANENT),
+	INSTANT(CardTypeType.SPELL),
+	SORCERY(CardTypeType.SPELL);
+	
+	private CardTypeType typetype;
+	CardType(CardTypeType typetype) {
+		this.typetype = typetype;
+	}
+	
+	public boolean isPermanent() {
+		return typetype == CardTypeType.PERMANENT;
+	}
+}
